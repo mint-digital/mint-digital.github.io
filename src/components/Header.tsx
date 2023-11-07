f3c036cc14eb6bbb09e9d740dab7afe20d254749
Johan Burell <johan.burell@valtech.com>
2023-11-07 01:45:39 +0100

    WIP: Fixed broken urls

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -127,22 +127,22 @@ export default function Header({
   switch(videoNumber) {
 //  switch(headProps.videoNumber) {
     case 1:       
-      videoFile = "img/700_F_638263369_S6IB0OWycFo8cxSvq22CpEVY4hzDSOmg_ST.mp4";   
+      videoFile = "../img/700_F_638263369_S6IB0OWycFo8cxSvq22CpEVY4hzDSOmg_ST.mp4";   
     break;
     case 2:       
-      videoFile = "img/test_redux.mp4";   
+      videoFile = "../img/test_redux.mp4";   
     break;
     case 3:       
-      videoFile = "img/test_redux2.mp4";   
+      videoFile = "../img/test_redux2.mp4";   
     break;
     case 4:       
-      videoFile = "img/pexels-carsten-link-14057038 (1080p).mp4";   
+      videoFile = "../img/pexels-carsten-link-14057038 (1080p).mp4";   
     break;
     case 5:       
-      videoFile = "img/forest.mp4";   
+      videoFile = "../img/forest.mp4";   
     break;
     default:
-      videoFile = "img/forest.mp4";
+      videoFile = "../img/forest.mp4";
   };
 
 	return (
